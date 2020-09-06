5b299fe38e6a9b6c93b64d14f897ac78f179e87d
Humus4theWin <33117449+Humus4theWin@users.noreply.github.com>
2020-09-06 15:11:32 +0200

    uptodate

---
+++ hue.js
@@ -0,0 +1,69 @@
+const axios = require('axios')
+
+let baseURL = "http://192.168.188.116/api/kYibW7kfMgNcVO8aOVU6-WhgDvk1JR7bWnwuagdb/"
+let state = {
+    groups:{
+        Zimmer:{
+            id:1,
+            state: undefined
+        }
+    },
+    scenes:{
+        Zocken:{
+            id:"J15Eq3ZI8o0xmDT",
+            state: undefined
+        },
+        Fire:{
+            id:"nnCxQ8vzAESYx6l",
+            state: undefined
+        },
+        DarkColorDreamm:{
+            id:"CENRDn4vAQf4hRf",
+            state: undefined
+        },
+        LightColorDreamm:{
+            id:"2hcllLNsL5HbqQI",
+            state: undefined
+        },
+        ChillPC:{
+            id:"sjr2mqbSaHvy8sJ",
+            state: undefined
+        },
+        Konzentrieren:{
+            id:"hGXtjijDGw6UFoL",
+            state: undefined
+        },
+        Hell:{
+            id:"ZYt9Y6Xri8ab1Qs",
+            state: undefined
+        },    
+    }
+}
+
+function activateScene(scene){
+    let url = "http://192.168.188.116/api/kYibW7kfMgNcVO8aOVU6-WhgDvk1JR7bWnwuagdb/groups/1/action"
+    let body ="{\"scene\": \""+scene.id+"\"}";
+    axios.put(url,body)
+}
+
+function getRoomState(scene){
+    axios.get("http://192.168.188.116/api/kYibW7kfMgNcVO8aOVU6-WhgDvk1JR7bWnwuagdb/groups/1/").then((res)=> {
+        if(state.groups.Zimmer.state.on!== res.data.action.on){
+
+        }    
+
+        state.groups.Zimmer.state=res.data.action;
+
+    })
+}
+function restoreState(scene){
+    axios.put("http://192.168.188.116/api/kYibW7kfMgNcVO8aOVU6-WhgDvk1JR7bWnwuagdb/groups/1/action",state.groups.Zimmer.state)
+}
+
+activateScene(state.scenes.Hell)
+
+setInterval(getRoomState,500)
+
+function getPowerStatus(){
+    axios.get("http://192.168.188.116/api/kYibW7kfMgNcVO8aOVU6-WhgDvk1JR7bWnwuagdb/groups/1/").then((res)=> state.groups.Zimmer.state=res.data.action)
+}
