ab42a1fb8436b15155081f8656c9b7015c1a2368
Linus Pfoch <linus@pfoch.de>
2021-04-17 16:51:09 +0200

    added frontend

---
+++ Frontend/hello-world/src/components/SelectButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="SelectButton">
+    <button onclick="sendCommand("+ type +"," + value")">
+      value
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectButton',
+  props: {
+    type: String,
+    value: String,
+    range: Array
+  }
+}
+
+function sendCommand(type, value){
+  let body = { }
+  body[type] = value
+  Vue.axios.post('http://192.168.188.67:81/vsx',body).then((response) => {
+      console.log(response.data)
+  })
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
